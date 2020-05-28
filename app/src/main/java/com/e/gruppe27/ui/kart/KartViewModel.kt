0acb02f21ce190d37f421e04f1514165a0816837
Jakobegeland <jakob.f.e@hotmail.no>
2020-05-28 19:16:59 +0200

    Kilder for kartViewModel

--- app/src/main/java/com/e/gruppe27/ui/kart/KartViewModel.kt
+++ app/src/main/java/com/e/gruppe27/ui/kart/KartViewModel.kt
@@ -1,5 +1,15 @@
 package com.e.gruppe27.ui.kart
 
+//KILDER
+//Polyline, point and poly graphics
+//https://developers.arcgis.com/labs/android/display-point-line-and-polygon-graphics/
+
+//Layers on map
+//https://developers.arcgis.com/labs/android/add-layers-to-a-map/
+
+//Simple 2D Map
+//https://developers.arcgis.com/labs/android/create-a-starter-app/
+
 
 import android.app.Application
 import android.graphics.Color
